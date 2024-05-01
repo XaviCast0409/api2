d9be2738bc20a439c7e1dc33d34fec7aaaac4cee
Xavimont0409 <xavier.mont.0409@gmail.com>
2024-05-01 10:05:31 -0500

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
-import express from "express"
-import dotenv from "dotenv"
-import router from "./routes"
+import express from "express";
+import dotenv from "dotenv";
+import router from "./routes";
 import cors from 'cors';
 import db from "./config/dbConnect";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-}));  
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(router)
+app.use(router);
+
+// Obtiene el puerto de la variable de entorno, si no está definido, utiliza el puerto 3000
+const port = process.env.APP_PORT ? parseInt(process.env.APP_PORT, 10) : 3000;
 
 db.sequelize.sync({ alter: true }).then(() => {
-  app.listen(process.env.APP_PORT, () => {
-    console.log(`${process.env.APP_NAME} on port ${process.env.APP_PORT}`);
-  })
-})
-
-
-
+  app.listen(port, "0.0.0.0", () => {
+    console.log(`${process.env.APP_NAME} on port ${port}`);
+  });
+});
 
