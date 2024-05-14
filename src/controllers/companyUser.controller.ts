aec0ac7fd5ba9518ce45a530281fab4a3f023cde
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-05-14 20:41:39 -0300

    chore: Update companyUser.controller.ts to handle payment and create CompanyUser

--- src/controllers/companyUser.controller.ts
+++ src/controllers/companyUser.controller.ts
@@ -6,27 +6,19 @@ import stripe from "../stripe/stripeConfig";
 export const createCompanyUser = async (req: Request, res: Response): Promise<Response> => {
   const { UserId, CompanyId }: CompanyUserInput = req.body;
 
-  try {   
+  try {
     const createCompanyUser = await db.CompanyUser.create({
       UserId, CompanyId
     });
 
     // pago de los 5 dolares
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: 5000,
-      currency: "usd",
-      customer: UserId.toString(), //custumerId
-      confirm: true, 
-      metadata: {
-        CompanyId: CompanyId.toString()
-      }
-    });
+
 
     return res.status(201).json({
       message: 'CompanyUser created successfully',
       status: 201,
       CompanyUser: createCompanyUser,
-      paymentIntent 
+
     });
   } catch (error) {
     console.error('Error creating company user:', error);
@@ -38,15 +30,45 @@ export const createCompanyUser = async (req: Request, res: Response): Promise<Re
 };
 
 export const createCompanyUserFunction = async (UserId: number, CompanyId: number): Promise<void> => {
-  try {   
+  try {
     console.log('Creating company user:', UserId, CompanyId);
-    
+    const findCompany = await db.Company.findOne({
+      where: { id: CompanyId }
+    });
+
+    // Obtener los métodos de pago de tipo tarjeta asociados a la compañía
+    const paymentMethods = await stripe.paymentMethods.list({
+      customer: findCompany.customerstripeId,
+      type: "card",
+    });
+
+    if (!paymentMethods.data.length) {
+      throw new Error("No card found for the company");
+    }
+
+    // Utiliza el primer método de pago de tarjeta encontrado
+    const paymentMethodId = paymentMethods.data[0].id;
+
+    // Crea el PaymentIntent con el método de pago de tarjeta
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 500,
+      currency: "usd",
+      customer: findCompany.customerstripeId,
+      payment_method: paymentMethodId, // Usa el ID del método de pago de tarjeta
+      confirm: true,
+      metadata: {
+        CompanyId: CompanyId.toString()
+      },
+      return_url: "http://localhost:5173/matchingform"
+    });
+    console.log('PaymentIntent created:', paymentIntent);
     const createCompanyUser = await db.CompanyUser.create({
-      UserId: UserId, CompanyId: CompanyId
+      UserId: UserId,
+      CompanyId: CompanyId
     });
     console.log('CompanyUser created successfully', createCompanyUser);
   } catch (error) {
-    console.error('Error creating company user:', error);
-    throw error;
+    console.error(error);
+    // Manejo de errores
   }
 };
