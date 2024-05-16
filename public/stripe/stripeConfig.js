1899cda88dbb2f67d86a640d6696ed991c65d803
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-05-16 19:02:16 -0300

    chore: Update stripeConfig.js to load API secret from environment variables

--- public/stripe/stripeConfig.js
+++ public/stripe/stripeConfig.js
@@ -2,7 +2,14 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const stripe_1 = __importDefault(require("stripe"));
-const stripe = new stripe_1.default("sk_test_51OzkqmP7so0IzTMyNLbj2rNtyis9WMgo4ExYARMBrW3Mwqu6NnqoSo9Vf2LsGcQxQvTpgK5bcm1FqsERiSUQluC100DoeIwWR8");
+const dotenv_1 = __importDefault(require("dotenv"));
+// Carga las variables de entorno desde el archivo .env
+dotenv_1.default.config();
+// Obtén el valor de la variable de entorno y proporciona un valor predeterminado si es `undefined`
+const apiSecretStripe = (_a = process.env.API_SECRET_STRIPE) !== null && _a !== void 0 ? _a : '';
+// Crea una nueva instancia de Stripe utilizando la variable de entorno
+const stripe = new stripe_1.default(apiSecretStripe);
 exports.default = stripe;
