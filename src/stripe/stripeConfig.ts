38bc5a48563ab4f40c4535aa5bf162c466c2df27
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-05-16 19:00:05 -0300

    chore: Update stripeConfig.ts to load API secret from environment variables

--- src/stripe/stripeConfig.ts
+++ src/stripe/stripeConfig.ts
@@ -1,5 +1,13 @@
 import Stripe from "stripe";
+import dotenv from "dotenv";
 
-const stripe = new Stripe("sk_test_51OzkqmP7so0IzTMyNLbj2rNtyis9WMgo4ExYARMBrW3Mwqu6NnqoSo9Vf2LsGcQxQvTpgK5bcm1FqsERiSUQluC100DoeIwWR8");
+// Carga las variables de entorno desde el archivo .env
+dotenv.config();
+
+// Obtén el valor de la variable de entorno y proporciona un valor predeterminado si es `undefined`
+const apiSecretStripe = process.env.API_SECRET_STRIPE ?? '';
+
+// Crea una nueva instancia de Stripe utilizando la variable de entorno
+const stripe = new Stripe(apiSecretStripe);
 
 export default stripe;
